4da4b081bb211fc0c9daa1e917eb995b7619ce20
OrozbekDev04 <https://github.com/OrozbekDev04/zoone.git>
2024-07-11 12:49:15 +0600

    Header is ready , here has new svg , new img . I dont add the trans languge

--- src/widgets/header/styles/style.ts
+++ src/widgets/header/styles/style.ts
@@ -1,3 +1,31 @@
 import { styled } from "@mui/material";
 
-export const Header = styled("div")(({ theme }) => ({}));
+export const Header = styled("div")(({ theme }) => ({
+    display:'flex',
+    alignItems:"center",
+    justifyContent:'space-between',
+    background:'#FFFFFF',
+    height:'87px',
+    padding:'0 10px '
+}));
+
+export const HeaderLogos = styled('div')(({theme}) => ({
+    display:'flex',
+    alignItems:'center',
+    gap:'10px',
+    
+}))
+
+export const INotice = styled('div')(({theme})=> ({
+    a:{
+        background:'#F0F0F0',
+        display:'flex',
+        alignItems:"center",
+        justifyContent:'center',
+        width:'50px',
+        heigth:'50px',
+        borderRadius:'50%',
+        padding:'15px 25px'
+    }
+   
+}))
