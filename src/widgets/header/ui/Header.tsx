96163b09392424d0b1804453310356fe1d3a3ea5
OrozbekDev04 <https://github.com/OrozbekDev04/zoone.git>
2024-07-12 10:40:45 +0600

    header is put

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import IconNotice from 'shared/assets/svg/IconNotice.svg';
 import logoAdmin from 'shared/assets/svg/logoAdmin.svg';
-import burger from 'shared/assets/svg/burger.svg';
 import { Header as HeaderStyle } from '../styles/style';
 import { HeaderLogos as HeaderLogos } from '../styles/style';
 import { INotice as INotice } from '../styles/style';
@@ -10,13 +9,14 @@ import { INotice as INotice } from '../styles/style';
 const Header = () => {
   return (
     <HeaderStyle>
-      <img src={burger} alt="burger" />
-      <HeaderLogos>
+      <p>Admin</p>
+      {/* <HeaderLogos>
         <INotice>
           <NavLink to='/'><img src={IconNotice} alt="iconNotice" /></NavLink>
         </INotice>
         <NavLink to='/'><img src={logoAdmin} alt="logAdmina" /></NavLink>
-      </HeaderLogos>
+      </HeaderLogos> */}
+      <p>Ф.И.О</p>
     </HeaderStyle>
   )
 }
