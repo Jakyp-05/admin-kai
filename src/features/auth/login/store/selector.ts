f132ad58088e881c405da3373fd81a6885b59185
Jakyp-05 <jakypjumagulov080gmail.com>
2024-08-07 17:37:34 +0600

    check

--- src/features/auth/login/store/selector.ts
+++ src/features/auth/login/store/selector.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export {};
-=======
 import { RootState } from "app/store";
 
 export const selectLogin = (state: RootState) => state.login.login;
->>>>>>> 40c8306a23c18f7f9209504f47391855092f2ac2
