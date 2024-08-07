1a064e67302f6c37de6a610719c9fdd281cb6971
ArzygulIm <arzygul.imankulova.kk@gmail.com>
2024-08-07 16:48:34 +0600

    delete imports

--- src/features/auth/login/store/selector.ts
+++ src/features/auth/login/store/selector.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-export {};
-=======
+
 import { RootState } from "app/store";
 
 export const selectLogin = (state: RootState) => state.login.login;
->>>>>>> 40c8306a23c18f7f9209504f47391855092f2ac2
